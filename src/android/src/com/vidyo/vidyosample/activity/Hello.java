674e144dff224ffe892a91f2faee2af4db381827
Austin Myers <austin@rinsoft.com>
2015-03-31 17:55:18 -0700

    exit button in Vidyo player exits the plugin. Hard coded Login for now

---
+++ src/android/src/com/vidyo/vidyosample/activity/Hello.java
@@ -0,0 +1,54 @@
+package com.vidyo.vidyosample.activity;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
+public class Hello extends CordovaPlugin {
+    public static String TAG = "Hello";
+
+    private CallbackContext callbackContext;
+    private JSONObject params;
+
+    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
+        this.params = args.getJSONObject(0);
+
+        if (action.equals("getPictures")) {
+            Intent intent = new Intent(cordova.getActivity(), VidyoSampleActivity.class);
+			intent.putExtra("server", "test456");
+            intent.putExtra("username", "test456");
+            intent.putExtra("password", "test456");
+            if (this.cordova != null) {
+                this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
+            }
+        }
+        return true;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		throw new RuntimeException("This is a crash");
+		/*
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            this.callbackContext.success("It works!");
+        } else if (resultCode == Activity.RESULT_CANCELED && data != null) {
+            String error = data.getStringExtra("ERRORMESSAGE");
+            this.callbackContext.error(error);
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            JSONArray res = new JSONArray();
+            this.callbackContext.success(res);
+        } else {
+            this.callbackContext.error("No images selected");
+        }
+		*/
+    }
+}
